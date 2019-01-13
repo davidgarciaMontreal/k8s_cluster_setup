1089355a593bad67fe38c9dee7ff395528b300b7
David Garcia-Campos <dgarciacampos@gmail.com>
2019-01-13 15:21:56 -0500

    Use Vagrant to create Cluster
    Much easier this way (:

---
+++ vagrant/vms/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+rm -rf /etc/yum.repos.d/kubernetes.repo || true
+sudo cp /tmp/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
+sudo systemctl restart sshd
+sudo yum install -y docker kubelet kubeadm kubectl kubernetes-cni
+sudo systemctl enable docker && systemctl start docker
+sudo systemctl enable kubelet && systemctl start kubelet
